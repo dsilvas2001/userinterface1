8c550f7df2c8b17bfeab04c5e442f028527e9576
dsilvas2001 <dsilvas@uteq.edu.ec>
2022-11-28 18:26:28 -0500

    1.6

--- app/src/main/java/com/demh/userinterface1/ActivityMostrar.kt
+++ app/src/main/java/com/demh/userinterface1/ActivityMostrar.kt
@@ -2,10 +2,28 @@ package com.demh.userinterface1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ActivityMostrar : AppCompatActivity() {
+
+    private lateinit var txtname : TextView
+    private lateinit var textelefono : TextView
+    private lateinit var textciudad : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar)
+
+        txtname = findViewById(R.id.textname)
+        textelefono = findViewById(R.id.textelefono)
+        textciudad = findViewById(R.id.textciudad)
+
+        val nameperson = intent.getStringExtra("NombreCompleto")
+        val city = intent.getStringExtra("Ciudad")
+        val phoneperson = intent.getStringExtra("Telefono")
+
+        txtname.text = "Nombres:  " + nameperson
+        textelefono.text = "Ciudad: " + city
+        textciudad.text = "Telefono: " +phoneperson
     }
 }
--- app/src/main/java/com/demh/userinterface1/MainActivity.kt
+++ app/src/main/java/com/demh/userinterface1/MainActivity.kt
@@ -7,17 +7,22 @@ import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var editCedula : EditText
     private  lateinit var editnameperson : EditText
+    private  lateinit var editTextDate : EditText
     private  lateinit var editciudad : EditText
     private  lateinit var editPhoneperson : EditText
     private  lateinit var editEmailperson : EditText
+
     private  lateinit var btn_acces : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        editCedula = findViewById(R.id.editCedula)
         editnameperson = findViewById(R.id.editnameperson)
+        editTextDate = findViewById(R.id.editTextDate)
         editciudad = findViewById(R.id.editciudad)
         editPhoneperson = findViewById(R.id.editPhoneperson)
         editEmailperson = findViewById(R.id.editEmailperson)
@@ -25,11 +30,12 @@ class MainActivity : AppCompatActivity() {
 
         btn_acces.setOnClickListener {
             startActivity(Intent(this,ActivityMostrar::class.java)
-                .putExtra("Nombre Completo",editnameperson.text.toString())
+                .putExtra("Cedulauser",editCedula.text.toString())
+                .putExtra("NombreCompleto",editnameperson.text.toString())
+                .putExtra("DateNacimiento",editTextDate.text.toString())
                 .putExtra("Ciudad",editciudad.text.toString())
                 .putExtra("Telefono",editPhoneperson.text.toString())
                 .putExtra("Correo",editEmailperson.text.toString())
-
             )
         }
 
